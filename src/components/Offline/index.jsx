32e4c6c39238f91741c4d20a85a0974223a1f4df
Lucka <48294719+lsevcikova@users.noreply.github.com>
2023-10-08 17:46:38 +0200

    updated offline component

--- src/components/Offline/index.jsx
+++ src/components/Offline/index.jsx
@@ -1,17 +1,36 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react'
 
 const Offline = () => {
-  const [isOnline, setIsOnline] = useState(navigator.onLine);
+  const [isOnline, setIsOnline] = useState(navigator.onLine)
+  useEffect(() => {
+    const handleOffline = () => {
+      setIsOnline(false)
+      console.log('Offline event detected')
+    }
+
+    const handleOnline = () => {
+      setIsOnline(true)
+      console.log('Online event detected')
+    }
+
+    window.addEventListener('offline', handleOffline);
+    window.addEventListener('online', handleOnline);
+
+    console.log('Component mounted');
+
+    return () => {
+      window.removeEventListener('offline', handleOffline);
+      window.removeEventListener('online', handleOnline);
+      console.log('Component unmounted');
+    };
+
+  }, []);
 
   if (isOnline) {
-    return (
-      <p>Všechno v pořádku</p>
-    )
-  };
+    return <p>Všechno v pořádku</p>
+  }
 
-  return (
-    <p>Jste offline!</p>
-  );
-};
+  return <p>Jste offline!</p>
+}
 
-export default Offline;
+export default Offline
